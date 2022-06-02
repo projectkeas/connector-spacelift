c8688f18200dc18b06d7b938589792c9755e5962
Stuart Blackler (@im5tu) <im5tu@users.noreply.github.com>
2022-06-02 15:00:30 +0100

    Initial version of the spacelift connector

---
+++ services/httpRequest/service.go
@@ -0,0 +1,59 @@
+package httpRequest
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"time"
+
+	log "github.com/projectkeas/sdks-service/logger"
+	"go.uber.org/zap"
+)
+
+func PostJson(uri string, payload interface{}, headers map[string]string) (int, string, error) {
+
+	jsonData, _ := json.Marshal(payload)
+	req, _ := http.NewRequest(http.MethodPost, uri, bytes.NewBuffer(jsonData))
+
+	headers["Content-Type"] = "application/json"
+	for key, value := range headers {
+		req.Header.Add(key, value)
+	}
+
+	start := time.Now()
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Logger.Error(err.Error(), zap.Error(err))
+		return resp.StatusCode, "", err
+	}
+
+	bytesReceived := 0
+
+	defer func() {
+		resp.Body.Close()
+		log.Logger.Debug(fmt.Sprintf("Sent request to '%s'...", uri), zap.Any("http", map[string]interface{}{
+			"statusCode":    resp.StatusCode,
+			"url":           uri,
+			"method":        "POST",
+			"latency":       time.Since(start).Milliseconds(),
+			"bytesSent":     len(jsonData),
+			"bytesReceived": bytesReceived,
+		}))
+	}()
+
+	responseBody, err := ioutil.ReadAll(resp.Body)
+	bytesReceived = len(responseBody)
+	if err != nil {
+		log.Logger.Error(err.Error(), zap.Error(err))
+		if resp.StatusCode == 400 {
+			return resp.StatusCode, string(responseBody), err
+		} else {
+			return resp.StatusCode, "", err
+		}
+	}
+
+	return resp.StatusCode, "", nil
+}
